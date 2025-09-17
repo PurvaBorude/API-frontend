80003a7f367fd3be3aa578933c19dc22ddccdf58
Purva Borude <borudepurva@gmail.com>
2025-09-17 18:00:13 +0530

    spa handling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 
 // Auth Page (switches between login, register, forgot/reset)
